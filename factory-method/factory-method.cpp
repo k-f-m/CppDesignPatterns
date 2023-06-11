efcec73d745f3fe3c1827a70b82f0ea914bbf879
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-06-12 00:09:34 +0200

    Restructure the comment block for readability

--- factory-method/factory-method.cpp
+++ factory-method/factory-method.cpp
@@ -1,7 +1,8 @@
 /*
  * This code shows how the Factory Method pattern can be implemented in C++.
  *
- * The Factory Method is a creational design pattern that provides an interface for creating objects in a superclass, but allows subclasses to alter the type of objects that will be created.
+ * The Factory Method is a creational design pattern that provides an interface
+ * for creating objects in a superclass, but allows subclasses to alter the type of objects that will be created.
  */
 
 #include <iostream>
