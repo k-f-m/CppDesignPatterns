a0af9e8397ad75524f73cdd7545bb80382c73e2a
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-07-21 23:59:27 +0200

    Add example for the Observer pattern

---
+++ observer/observer.cpp
@@ -0,0 +1,55 @@
+/*
+* This example code shows how the Observer pattern can be implemented in C++.
+*
+* The Observer pattern is a behavioral design pattern that lets us define a subscription 
+* mechanism to notify multiple objects about any events that happen to the object they’re observing. 
+* Observer defines a one-to-many dependency between objects so that when one object changes state, 
+* all its dependents are notified and updated automatically.
+*/
+
+#include <iostream>
+#include <vector>
+
+class IObserver {
+public:
+    virtual ~IObserver() {}
+    virtual void update() = 0;
+};
+
+class ISubject {
+public:
+    virtual ~ISubject() {}
+    virtual void attach(IObserver* observer) = 0;
+    virtual void detach(IObserver* observer) = 0;
+    virtual void notify() = 0;
+};
+
+class ConcreteObserver : public IObserver {
+public:
+    ConcreteObserver(const std::string& name) : name_(name) {}
+    void update() override { std::cout << "Observer " << name_ << " is notified." << std::endl; }
+private:
+    std::string name_;
+};
+
+class ConcreteSubject : public ISubject {
+public:
+    void attach(IObserver* observer) override { observer_list.push_back(observer); }
+    void detach(IObserver* observer) override { observer_list.erase(std::remove(observer_list.begin(), observer_list.end(), observer), observer_list.end()); }
+    void notify() override {
+        for (auto& observer : observer_list) {
+            observer->update();
+        }
+    }
+private:
+    std::vector<IObserver*> observer_list;
+};
+
+int main() {
+    ConcreteSubject subject;
+    ConcreteObserver observer1("1");
+    ConcreteObserver observer2("2");
+    subject.attach(&observer1);
+    subject.attach(&observer2);
+    subject.notify();
+}
